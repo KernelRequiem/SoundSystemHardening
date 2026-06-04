89a1ff2239033521967159889053cc973c1ebcde
menardrop-cpu <menardrop@gmail.com>
2026-06-05 00:00:52 +0200

    fix: réactiver sitemap avec version compatible

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://soundsystemhardening.fr',
-  integrations: [tailwind(), mdx()],
+  integrations: [tailwind(), mdx(), sitemap()],
   markdown: {
     shikiConfig: {
       theme: 'github-dark',
