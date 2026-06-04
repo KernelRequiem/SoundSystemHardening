a0bbed7274d415b64ecd87e5f1b4584af0c1953e
menardrop-cpu <menardrop@gmail.com>
2026-06-05 00:02:43 +0200

    fix: désactiver sitemap définitivement

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,10 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://soundsystemhardening.fr',
-  integrations: [tailwind(), mdx(), sitemap()],
+  integrations: [tailwind(), mdx()],
   markdown: {
     shikiConfig: {
       theme: 'github-dark',
