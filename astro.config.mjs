7e65f332c7320dd801f5b72b2e2018ee00ca18d3
menardrop-cpu <menardrop@gmail.com>
2026-06-04 21:58:37 +0200

    feat: ajouter sitemap @astrojs/sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
+  site: 'https://soundsystemhardening.fr',
   integrations: [tailwind(), mdx()],
   markdown: {
     shikiConfig: {
